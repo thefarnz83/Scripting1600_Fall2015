1a64c3eeb7d086159010f0a7372f67361567bf42
Michael Farnsworth <thefarnz83@gmail.com>
2015-09-17 08:35:56 -0600

    if statements and conventions

---
+++ Scripting_Fall_2015/Assets/ifstatements.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifstatements : MonoBehaviour
+{
+	public float coffeeTemperature = 85.0f;
+	public float hotLimitTemperature = 70.0f;
+	public float coldLimitTemperature = 40.0f;
+	
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+		
+		coffeeTemperature -= Time.deltaTime * 20;
+	}
+	
+	
+	void TemperatureTest ()
+	{
+		// If the coffee's temperature is greater than the hottest drinking temperature...
+		if(coffeeTemperature > hotLimitTemperature)
+		{
+			// ... do this.
+			print("Coffee is too hot.");
+		}
+		// If it isn't, but the coffee temperature is less than the coldest drinking temperature...
+		else if(coffeeTemperature < coldLimitTemperature)
+		{
+			// ... do this.
+			print("Coffee is too cold.");
+		}
+		// If it is neither of those then...
+		else
+		{
+			// ... do this.
+			print("Coffee is just right.");
+		}
+	}
+}
---
+++ Scripting_Fall_2015/Assets/ifstatementstest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifstatementstest : MonoBehaviour
+{
+	public float airspeed = 0f;
+	public float increasespeed = 60f;
+
+	void Update () 
+	{
+		//if (Input.GetKeyDown (KeyCode.Space))
+			detectairspeed ();
+
+		airspeed += Time.deltaTime * 10;
+	}
+
+	void detectairspeed ()
+	{
+		if (airspeed <= increasespeed) 
+		{
+			print ("Increase Speed!");
+		}
+
+		else
+		{
+			print ("Take Off!");
+		}
+	}
+}
